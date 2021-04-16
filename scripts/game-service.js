ddc5fcae73100f344d5e542ee0ef66ed2671d9b7
Kevin Loeffler <hi@kevinloeffler.ch>
2021-04-16 13:00:15 +0200

    Added eslint disable for 'for of' loops

--- scripts/game-service.js
+++ scripts/game-service.js
@@ -1,13 +1,14 @@
 // const DELAY_MS = 1000; -> For Testat 2
 
-import {hands} from "./choices.js";
-import RoundResult from "./roundResult.js";
+import {hands} from './choices.js'
+import RoundResult from './roundResult.js'
 
 export function aiPickHand() {
     return hands[Math.floor(Math.random() * hands.length)]
 }
 
 export function findHand(target) {
+    // eslint-disable-next-line no-restricted-syntax
     for (const hand of hands) {
         if (target === hand.name) return hand
     }
@@ -15,9 +16,9 @@ export function findHand(target) {
 }
 
 const resultsTable = {
-    '1': ['Win', 'Congrats, you won!'],
-    '0': ['Tie', 'Its a tie.'],
-    '-1': ['Loss', 'Oh no, you lost.']
+    1: ['Win', 'Congrats, you won!'],
+    0: ['Tie', 'Its a tie.'],
+    '-1': ['Loss', 'Oh no, you lost.'],
 }
 
 export function evaluateHand(player, result, hand, opponent) {
