47016864f4fd97ec0c3ce380ca34d532d9b2011a
Kevin Loeffler <hi@kevinloeffler.ch>
2021-04-16 11:46:02 +0200

    Removed unused functions

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -1,27 +1,14 @@
-import {Player} from './player.js'
-
-export const leaderboard = {
+const leaderboard = {
 
     localPlayers: [],
     serverPlayers: [], // for testat part 2
 
-    addPlayer: function(player, mode) {
+    addPlayer(player, /*mode*/) {
         // check mode in the future
         this.localPlayers.push(player)
     },
 
-    getPlayer: function(mode) {
-        // check mode in the future
-        return this.localPlayers
-    },
-
-    checkPlayer: function(player, mode) {
-        // check mode in the future
-        const p = this.localPlayers.find(player)
-        return p ? p : null
-    },
-
-    checkPlayerName: function(name, mode) {
+    checkPlayerName(name, /*mode*/) {
         // check mode in the future
         for (const player of this.localPlayers) {
             if (player.nickname === name) {
@@ -31,13 +18,15 @@ export const leaderboard = {
         return null
     },
 
-    sortPlayers: function(a, b) {
+    sortPlayers(a, b) {
         if (a.score < b.score) {
             return 1
-        } else if (a.score > b.score) {
+        }
+        if (a.score > b.score) {
             return -1
         }
         return 0
-    }
-
+    },
 }
+
+export default leaderboard
