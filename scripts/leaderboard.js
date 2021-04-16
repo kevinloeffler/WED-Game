ac2f3ee833864c94790883322e0331f19e2d4e80
Kevin Loeffler <hi@kevinloeffler.ch>
2021-04-16 13:00:04 +0200

    Added eslint disable for 'for of' loops

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -3,13 +3,14 @@ const leaderboard = {
     localPlayers: [],
     serverPlayers: [], // for testat part 2
 
-    addPlayer(player, /*mode*/) {
+    addPlayer(player) {
         // check mode in the future
         this.localPlayers.push(player)
     },
 
-    checkPlayerName(name, /*mode*/) {
+    checkPlayerName(name) {
         // check mode in the future
+        // eslint-disable-next-line no-restricted-syntax
         for (const player of this.localPlayers) {
             if (player.nickname === name) {
                 return player
