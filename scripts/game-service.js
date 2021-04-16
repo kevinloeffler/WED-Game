31caf7f8382522e405ee88e2896455b44d163d34
Kevin Loeffler <hi@kevinloeffler.ch>
2021-04-16 11:40:41 +0200

    Added methods from main.js

--- scripts/game-service.js
+++ scripts/game-service.js
@@ -1,39 +1,28 @@
-const DELAY_MS = 1000;
-const rankings = {
-    // Test Rankings
-    Markus: {name: 'Markus', win: 3, lost: 6},
-    Michael: {name: 'Michael', win: 4, lost: 5},
-    Lisa: {name: 'Lisa', win: 4, lost: 5},
-};
-export const HANDS = ['scissors', 'stone', 'paper'];
+// const DELAY_MS = 1000; -> For Testat 2
 
-function aiPickHand() {
-    return HANDS[Math.floor(Math.random() * HANDS.length)]
+import {hands} from "./choices.js";
+import RoundResult from "./roundResult.js";
+
+export function aiPickHand() {
+    return hands[Math.floor(Math.random() * hands.length)]
 }
 
-
-
-/*
-
-let isConnectedState = false;
-
-export function setConnected(newIsConnected) {
-    isConnectedState = Boolean(newIsConnected);
+export function findHand(target) {
+    for (const hand of hands) {
+        if (target === hand.name) return hand
+    }
+    return null
 }
 
-export function isConnected() {
-    return isConnectedState;
+const resultsTable = {
+    '1': ['Win', 'Congrats, you won!'],
+    '0': ['Tie', 'Its a tie.'],
+    '-1': ['Loss', 'Oh no, you lost.']
 }
 
-export function getRankings(rankingsCallbackHandlerFn) {
-    setTimeout(() => rankingsCallbackHandlerFn(rankings), DELAY_MS);
+export function evaluateHand(player, result, hand, opponent) {
+    const arr = resultsTable[result]
+    player.addToHistory(new RoundResult(arr[0], hand, opponent))
+    if (result === 1) { player.addWin() }
+    return arr[1]
 }
-
-export function evaluateHand(playerName, playerHand, didWinHandlerCallbackFn) {
-    // todo: replace calculation of didWin and update rankings while doing so.
-    // optional: in local-mode (isConnected == false) store rankings in the browser localStorage https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API
-    const didWin = Boolean(Math.floor(Math.random() * 2));
-    setTimeout(() => didWinHandlerCallbackFn(didWin), DELAY_MS);
-}
-
-*/
